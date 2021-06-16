ab521cd737318a0739a337fcbcc8e5d95b98245b
itay47 <Itaylcars@hotmail.com>
2021-06-16 09:15:22 +0300

    created Telegram library module

---
+++ jenkins-libs/Telegram.groovy
@@ -0,0 +1,6 @@
+// this is a Telegram sender module
+// How to build jenkins libraries:
+//   https://medium.com/android-news/how-to-build-libraries-for-jenkins-pipelines-1772aa0da53d
+// How to integrate Telegram with jenkins:
+//   https://medium.com/android-news/integrate-jenkins-with-telegram-1e5af7765d8
+
